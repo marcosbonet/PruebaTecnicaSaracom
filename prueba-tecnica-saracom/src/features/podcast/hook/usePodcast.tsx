a7224be2c99deef5e3cd44fc94e7e4244d050949
marcos <marcos.bonet@live.com.ar>
2023-01-06 20:04:55 +0100

    custon hook ready

--- prueba-tecnica-saracom/src/features/podcast/hook/usePodcast.tsx
+++ prueba-tecnica-saracom/src/features/podcast/hook/usePodcast.tsx
@@ -1,7 +1,21 @@
-// import { useDispatch } from "react-redux";
+import { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Repository } from '../repository/repository';
+import { rootState } from '../store/store';
+import * as ac from '../reducer/action.creator';
 
-// export const usePodcast =()=>{
-//     const podcast= useSelector ((state: rootState)=> state.player);
-//     const dispatcher= useDispatch();
+export const usePodcast = () => {
+    const podcast = useSelector((state: rootState) => state.podcast);
+    const dispatcher = useDispatch();
+    const apiPodcast = useMemo(() => new Repository(), []);
+    useEffect(() => {
+        apiPodcast.getAll().then((response) => {
+            console.log(response, 'hola');
+            dispatcher(ac.getAllActionCreator(response));
+        });
+    }, [apiPodcast, dispatcher]);
 
-// }
+    return {
+        podcast,
+    };
+};
