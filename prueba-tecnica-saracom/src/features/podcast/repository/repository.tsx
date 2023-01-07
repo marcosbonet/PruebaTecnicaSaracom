abc55836b7a2fae6b62a9390595c1a867d1dbcb2
marcos <marcos.bonet@live.com.ar>
2023-01-07 15:04:07 +0100

    respotory fixed

--- prueba-tecnica-saracom/src/features/podcast/repository/repository.tsx
+++ prueba-tecnica-saracom/src/features/podcast/repository/repository.tsx
@@ -6,7 +6,7 @@ export class Repository {
         this.URL =
             'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
     }
-    getAll(): Promise<Ipodcast> {
+    getAll(): Promise<Array<Ipodcast>> {
         return fetch(
             'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json',
             {
@@ -16,15 +16,39 @@ export class Repository {
             .then((response) => {
                 return response.json();
             })
-            .then((response) => {
-                return response.feed;
+            .then(({ feed: { entry } }) => {
+                const podcasts = entry.map((podcast: any) => {
+                    return {
+                        images: podcast['im:image'][2].label,
+                        artist: podcast['im:artist'].label,
+                        name: podcast['im:name'].label,
+                    };
+                });
+                return podcasts;
             });
     }
 
-    query(value: string): Promise<Ipodcast> {
-        const url = URL + `${value}`;
-        return fetch(url, { method: 'GET' }).then((response) => {
-            return response.json();
-        });
-    }
+    // Search(dataSearch: string): Promise<Array<Ipodcast>> {
+    //     const url =
+    //         'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+    //     const search = `?q=${dataSearch}`;
+    //     const completeUrl = url + search;
+    //     const result = fetch(completeUrl).then((response) => {
+    //         return response.json();
+    //     });
+    //     return result;
+    // }
+    // query(key: string, value: string): Promise<Array<Ipodcast>> {
+    //     return fetch(`${this.URL}/${key}/${value}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-type': 'application/json',
+    //             Authorization: `Bearer ${localStorage.getItem('token')}`,
+    //         },
+    //     })
+    //         .then((res) => res.json())
+    //         .catch((error) => {
+    //             return error;
+    //         });
+    // }
 }
