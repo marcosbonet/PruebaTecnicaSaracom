0074399695cb8c1e54293b2ce97bfc33c8a870db
marcos <marcos.bonet@live.com.ar>
2023-01-08 20:35:20 +0100

    item and list podcast fixed

--- prueba-tecnica-saracom/src/infrastructure/component/item.podcast.tsx
+++ prueba-tecnica-saracom/src/infrastructure/component/item.podcast.tsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import { Ipodcast } from '../../features/podcast/entities/entities';
 
 export function ItemPodcast({ item }: { item: Ipodcast }) {
     return (
         <>
-            <img src={item.images} alt={item.name} width="200px"></img>
-            <p>{item.name}</p>
-            <p>{item.artist}</p>
+            <Link to={`/podcast/${item.id}`}>
+                <div>
+                    <img src={item.images} alt={item.name} width="200px"></img>
+                    <p>{item.name}</p>
+                    <p>{item.artist}</p>
+                </div>
+            </Link>
         </>
     );
 }
--- prueba-tecnica-saracom/src/infrastructure/component/list.podcast.tsx
+++ prueba-tecnica-saracom/src/infrastructure/component/list.podcast.tsx
@@ -2,6 +2,7 @@ import { Search } from '../component/search';
 import { Ipodcast } from '../../features/podcast/entities/entities';
 import { usePodcast } from '../../features/podcast/hook/usePodcast';
 import { ItemPodcast } from './item.podcast';
+import { useNavigate } from 'react-router-dom';
 
 export const ListPodcast = () => {
     const { podcast } = usePodcast();
